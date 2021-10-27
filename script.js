ef4beddfe70ee983a86d8e05a772db9d7b6aa119
Rohfel <rohfeladyaraka@gmail.com>
2021-10-27 20:09:59 +0700

    The map now will redirect you based on the selected island.

---
+++ script.js
@@ -0,0 +1,27 @@
+function gotoSumatraPage() {
+	window.location.href = "islands/sumatra.html";
+}
+
+function gotoKalimantanPage() {
+	window.location.href = "islands/kalimantan.html";
+}
+
+function gotoJawaPage() {
+	window.location.href = "islands/jawa.html";
+}
+
+function gotoSulawesiPage() {
+	window.location.href = "islands/sulawesi.html";
+}
+
+function gotoSundaKecilPage() {
+	window.location.href = "islands/sundakecil.html";
+}
+
+function gotoMalukuPage() {
+	window.location.href = "islands/maluku.html";
+}
+
+function gotoPapuaPage() {
+	window.location.href = "islands/papua.html";
+}
