bde223c5b17dd5e785c24e87682a78350b9d75db
Reubenthekiwi <rpkusuma@outlook.com>
2021-10-28 16:44:44 +0700

    Fixed the code when clicking the map

--- script.js
+++ script.js
@@ -1,27 +1,3 @@
-function gotoSumatraPage() {
-	window.location.href = "islands/sumatra.html";
-}
-
-function gotoKalimantanPage() {
-	window.location.href = "islands/kalimantan.html";
-}
-
-function gotoJawaPage() {
-	window.location.href = "islands/jawa.html";
-}
-
-function gotoSulawesiPage() {
-	window.location.href = "islands/sulawesi.html";
-}
-
-function gotoSundaKecilPage() {
-	window.location.href = "islands/sundakecil.html";
-}
-
-function gotoMalukuPage() {
-	window.location.href = "islands/maluku.html";
-}
-
-function gotoPapuaPage() {
-	window.location.href = "islands/papua.html";
+function gotoPage(page) {
+	window.location.href = page;
 }
