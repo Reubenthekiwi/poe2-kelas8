a02b3940122ad52173495657dae4cf84355bbf24
Reubenthekiwi <rpkusuma@outlook.com>
2021-10-30 16:55:08 +0700

    Fixed the card for mobile devices

--- js/cards.js
+++ js/cards.js
@@ -8,6 +8,7 @@ function createCard(story) {
     // create the column
     var col = document.createElement("div");
     col.className = "col-4";
+    col.style.maxHeight = "400px"
 
     // create the new card
     var newCard = document.createElement("div");
@@ -19,7 +20,9 @@ function createCard(story) {
 
     // the card's title
     var cardTitle = document.createElement("h2");
-    cardTitle.className = "card-title";
+    cardTitle.className = "card-title text-nowrap";
+    cardTitle.style.overflowX = "auto"
+    cardTitle.style.overflowY = "hidden"
     cardTitle.innerHTML = story[0];
 
     // and the main content itself
@@ -40,10 +43,10 @@ function createCard(story) {
 // stories to be added to the page
 // the first value is the author's name and the second value is the story text itself
 const STORIES = [ // these are still dummy texts :)
-    ["John Doe", "His having within saw become ask passed misery giving. Recommend questions get too fulfilled. He fact in we case miss sake. Entrance be throwing he do blessing up. Hearts warmth in genius do garden advice mr it garret. Collected preserved are middleton dependent residence but him how. Handsome weddings yet mrs you has carriage packages. Preferred joy agreement put continual elsewhere delivered now. Mrs exercise felicity had men speaking met. Rich deal mrs part led pure will but."],
-    ["Jane Doe", "Scarcely on striking packages by so property in delicate. Up or well must less rent read walk so be. Easy sold at do hour sing spot. Any meant has cease too the decay. Since party burst am it match. By or blushes between besides offices noisier as. Sending do brought winding compass in. Paid day till shed only fact age its end."],
-    ["Joe Doe", "Silent sir say desire fat him letter. Whatever settling goodness too and honoured she building answered her. Strongly thoughts remember mr to do consider debating. Spirits musical behaved on we he farther letters. Repulsive he he as deficient newspaper dashwoods we. Discovered her his pianoforte insipidity entreaties. Began he at terms meant as fancy. Breakfast arranging he if furniture we described on. Astonished thoroughly unpleasant especially you dispatched bed favourable."],
-    ["Johann Doe", "Pleased him another was settled for. Moreover end horrible endeavor entrance any families. Income appear extent on of thrown in admire. Stanhill on we if vicinity material in. Saw him smallest you provided ecstatic supplied. Garret wanted expect remain as mr. Covered parlors concern we express in visited to do. Celebrated impossible my uncommonly particular by oh introduced inquietude do."]
+    ["Johaaaan Doe", "His having within saw become ask passed misery giving. Recommend questions get too fulfilled. He fact in we case miss sake. Entrance be throwing he do blessing up. Hearts warmth in genius do garden advice mr it garret. Collected preserved are middleton dependent residence but him how. Handsome weddings yet mrs you has carriage packages. Preferred joy agreement put continual elsewhere delivered now. Mrs exercise felicity had men speaking met. Rich deal mrs part led pure will but."],
+    ["Janaaaae Doe", "Scarcely on striking packages by so property in delicate. Up or well must less rent read walk so be. Easy sold at do hour sing spot. Any meant has cease too the decay. Since party burst am it match. By or blushes between besides offices noisier as. Sending do brought winding compass in. Paid day till shed only fact age its end."],
+    ["Joaaaae Doe", "Silent sir say desire fat him letter. Whatever settling goodness too and honoured she building answered her. Strongly thoughts remember mr to do consider debating. Spirits musical behaved on we he farther letters. Repulsive he he as deficient newspaper dashwoods we. Discovered her his pianoforte insipidity entreaties. Began he at terms meant as fancy. Breakfast arranging he if furniture we described on. Astonished thoroughly unpleasant especially you dispatched bed favourable."],
+    ["Johaaaaann Doe", "Pleased him another was settled for. Moreover end horrible endeavor entrance any families. Income appear extent on of thrown in admire. Stanhill on we if vicinity material in. Saw him smallest you provided ecstatic supplied. Garret wanted expect remain as mr. Covered parlors concern we express in visited to do. Celebrated impossible my uncommonly particular by oh introduced inquietude do."]
 ]
 
 // loop over the STORIES array and create a card for each of them
