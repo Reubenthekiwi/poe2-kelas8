27979630b24323fccff917ffec80897a8a717616
Reubenthekiwi <rpkusuma@outlook.com>
2021-11-11 18:04:41 +0700

    Completed the data for the other islands

--- js/chart.js
+++ js/chart.js
@@ -25,13 +25,13 @@ function createChart(context, islandName, islandData) {
 
 // the chart data for every islands
 const DATA = {
-    Sumatra: [66.3, 26.9, 4.31, 2.35, 0.11, 0.03],
-    Jawa: [97.26, 1.7, 0.76, 0.18, 0.11, 0.01],
-    Kalimantan: [],
-    Sulawesi: [],
-    SundaKecil: [],
-    Maluku: [],
-    Papua: [],
+    Sumatra: [66.30, 26.90, 4.31, 2.35, 0.11, 0.03],
+    Jawa: [97.26, 1.70, 0.76, 0.18, 0.11, 0.01],
+    Kalimantan: [78.23, 8.99, 8.86, 1.94, 0.27, 0.23],
+    Sulawesi: [80.89, 15.70, 1.56, 0.17, 1.36, 0.01],
+    SundaKecil: [38.76, 12.7, 18.2, 0.33, 29.77, 0.01],
+    KepulauanMaluku: [63.57, 32.15, 3.70, 0.01, 0.17, 0.01],
+    Papua: [20.65, 63.00, 15.42, 0.06, 0.09, 0.003],
 }
 
 // create the chart itself
